8d86264cf135e9cffe909e1d1986ccd159606680
fan <1291577591@qq.com>
2022-06-08 03:43:44 +0800

    third commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("测试");
         System.out.println("测试2");
+        System.out.println("测试3");
     }
 }
